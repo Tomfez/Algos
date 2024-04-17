a736f857102f7bf8744ecf214c242a98c3afada1
Thomas Fernez <thomas.fernez059@gmail.com>
2024-04-17 12:05:08 +0200

    First commit

---
+++ arrays/selection_sort.mjs
@@ -0,0 +1,22 @@
+const arr = [11, 15, 22, 1, 3, 6]
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+
+        if (i !== minIndex) {
+            const tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
+        }
+    }
+}
+
+selectionSort(arr);
+console.log(arr);
