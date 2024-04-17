bda57788ec4fe7e3482d6b14048dde0a456c83ab
Thomas Fernez <thomas.fernez059@gmail.com>
2024-04-17 12:24:37 +0200

    Tri à bulles

---
+++ arrays/bubble_sort.mjs
@@ -0,0 +1,23 @@
+const arr = [11, 15, 22, 1, 3, 6]
+
+function bubbleSort(arr) {
+    let length = arr.length;
+    let swap;
+
+    do {
+        swap = false;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                const tmp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = tmp;
+                swap = true;
+            }
+        }
+        length--;
+    } while (swap);
+}
+
+bubbleSort(arr);
+console.log(arr);
